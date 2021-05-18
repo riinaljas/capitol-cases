f22c65d51ef18bc317c22244c8a928346b4fa37a
riinaljas <aljasriin@gmail.com>
2021-05-18 14:01:45 -0400

    update pdf script

--- pdfs.R
+++ pdfs.R
@@ -84,8 +84,9 @@ myfiles <- paste0("complaints/", myfiles) %>%
 ## create keywords----
 keywords = "fuentes|extremist|supremacy|russia|germany|russian imperial movement|imperial| rim |identitarian|great replacement|generation identity|identitarianist|accelerationism|accelerationist|europe|europeans|europe|international|three percent|patriot front|radical|ukraine|france|french|qanon|azov|military|paramilitary|ohiomilitia|extreme|extremist|nazi|identity movement|generation|atomwaffen|aryan renaissance society|awd|battalion|black pigeon speaks|blood & honour|blue awakening|boogaloo|british|combat 18|daily Stormer|defend evropa|holy land foundation|generation evropa|movement against illegal migration|national action|national socialist underground|nordic resistance movement|order of nine angles|patriotic europeans against the islamization of the west|rise above movement| r.a.m. | ram |the base |sonnenkrieg division|feuerkrieg fivision| fkd |american renaissance|countercurrents|groyper|altright corporation|stanza|awakening|polish national day|american renaissance conference|financed|funded|funding|donation|alt right|far right"
 intkeywords = "abroad|foreign|russia|germany|russian imperial movement|imperial| rim |identitarian|great replacement|generation identity|identitarian|acceleration|europe|european|ukraine|france|french|azov|identity movement|generation|atomwaffen|aryan renaissance society|awd|battalion|black pigeon speaks|blood & honour|blue awakening|boogaloo|british|combat 18|daily Stormer|defend evropa|holy land foundation|generation evropa|movement against illegal migration|national action|national socialist underground|nordic resistance movement|order of nine angles|patriotic europeans against the islamization of the west|rise above movement| r.a.m. | ram |the base |sonnenkrieg|feuerkrieg| fkd |american renaissance|countercurrents|groyper|altright corporation|stanza|awakening|polish national day|american renaissance conference|financed|funded|funding|donation|international airport|investigate international and domestic terrorism|international|fundrais"
-symbolkeywords <- "racial|nsc-131|nsc131|symbol|auschwitz|6mwe|six million|6 million|jew| race |kekistan|frog|vietnam|confederacy|vdare"
+symbolkeywords <- " race |racial|nsc-131|nsc131|symbol|auschwitz|6mwe|six million|6 million|jew| race |kekistan|frog|vietnam|confederacy|vdare"
 susannekeywords <- "molotov|gun|guns|zip ties|taser|pitchfork| bear spray |spray|pole|fire|extinguisher|weapon|armed|gear|stick|firearm|club|axe|handle"
+race <- " race | racial |far-right|alt-right|fuentes|proud boys|oath keepers|three percent|patriot front|extreme"
 
 textdf <- data.frame(page = as.character(), 
                      name = as.character(), 
@@ -116,13 +117,7 @@ for (myfile in myfiles$file){
     
 }
 
-
-errorfiles <- myfiles %>% 
-  anti_join(textdf)
-
-race <- " race | racial |far-right|alt-right|fuentes|proud boys|oath keepers|three percent|patriot front|extreme"
-
-of_interest2 <- textdf %>% 
+of_interest <- textdf %>% 
   filter(has_keyword == "YES") %>% 
   count(name, keyword) %>% 
   select(-n) %>% 
@@ -137,254 +132,29 @@ of_interest2 <- textdf %>%
            str_remove_all('\\)')) %>% 
   filter(!keyword %in% c("investigate international and domestic terrorism", "international airport", "terrace"))
 
-symbolkeywords <- "racial|nsc-131|nsc131|symbol|auschwitz|6mwe|six million|6 million|jew| race |kekistan|frog|vietnam|confederacy|vdare"
+latest_same_file <- read_csv(
+  dir(pattern = "keywords_")
+)
+test <- of_interest %>% 
+  mutate(name = name %>% 
+           str_remove_all("(id)\\d\\d\\d_|(id)\\d\\d_")
+  )
+is_updated <- test %>% 
+  anti_join(latest_same_file, by = "name") %>% 
+  mutate(update_date = Sys.Date())
+
+is_not_updated <-test %>% 
+  anti_join(is_updated, by = "name") %>% 
+  mutate(update_date = str_extract_all(dir(pattern = "keywords_"),
+                                       "\\d\\d\\d\\d-\\d\\d-\\d\\d") %>% 
+           unlist() %>%
+           as.Date())
+of_ineterest <- bind_rows(is_updated, is_not_updated)
+
+f <- dir(pattern = "keywords_")
+# remove the files
+file.remove(f)
+
+
 write_csv(of_interest, paste0("keywords_", Sys.Date()))
 
-of_interest2 %>% anti_join(read_csv("keywords_2021-04-06")) %>% unique() %>% view()
-
-#test <- myfiles[8]
-downloadfiles <- downloadfiles %>% 
-  mutate(rank = rownames(downloadfiles))
-
-
-### susanne keywords -----
-
-
-for (myfile in myfiles$file){
-  temp <- pdf_text(myfile) %>% 
-    unlist() %>% 
-    as.data.frame() %>% 
-    rename(page = 1) %>% 
-    mutate(page = page %>% 
-             tolower() %>% 
-             trimws(which = c("both")) %>% 
-             str_replace_all("\n", " ") %>% 
-             as.character(),
-           name = myfile,
-           has_keyword = ifelse(str_detect(page, susannekeywords)==T, 
-                                "YES",
-                                "NO"),
-           keyword = str_match_all(page, susannekeywords) %>% 
-             as.character()
-    )
-  print(myfile)
-  
-  textdf <- bind_rows(textdf, temp)
-  
-}
-
-of_sus_int <- textdf %>% 
-  filter(has_keyword == "YES") %>% 
-  count(name, keyword) %>% 
-  select(-n) %>% 
-  mutate(person = str_extract(name, "(.*?) -") %>% 
-           str_remove_all("complaints/") %>% 
-           str_remove_all(" -")) %>% 
-  mutate(keyword = keyword %>% 
-           str_replace_all('c\\(|\\)|\\"', ""))
-
-of_sus_int <- of_sus_int %>% 
-  mutate(keyword = keyword %>% 
-           str_remove_all('"|[,]') %>% 
-           trimws())
-
-of_sus_int <- of_sus_int %>% 
-  mutate(split = str_split(keyword, " ")) %>% 
-  mutate(split = map(.$split, ~ unique(.x))) %>% 
-  mutate(split = map_chr(.$split, ~paste(.x, collapse = " "))) %>%
-  mutate(keyword = split %>% 
-           str_replace_all(" ", ", ")) %>% 
-  select(-split) %>% 
-  unique()
-
-cols = paste0("columname", c(1:7)) %>% as.character()
-
-of_sus_int  <- of_sus_int %>% 
-  separate(keyword, into = cols) %>% unique() %>%
-  pivot_longer(c(2:8), names_repair = "unique") %>% 
-  select(-2, -3) %>% 
-  filter(!is.na(value)) %>% 
-  unique()
-
-
-of_sus_int <- of_sus_int %>% 
-  rename(keyword = value, 
-         link = 1) 
-  
-test <- of_sus_int %>% 
-  pivot_wider(values_from = keyword, names_from = link) %>% 
-  pivot_longer(everything()) %>% 
-  rename(keyword = value) %>% 
-  mutate(keyword = keyword)
-
-#take out periods to make it easier to take out sentences
-periodwords <- c("U\\.S\\." = "US", 
-                 "p\\.m\\." = "pm", 
-                 "a\\.m\\.", "am")
-                 
-x <- pdf_text("complaints/Abual-Ragheb - Affidavit.pdf") %>% 
-  as.tibble() %>% 
-  rename(page = 1)
-
-x$page <- str_replace_all(x$page, c("U\\.S\\." = "US", 
-                                 "p\\.m\\." = "pm", 
-                                 "a\\.m\\." = "am", 
-                                 "d\\.c\\." = "dc"))
-  
-### extract sentences with words in them with page numbers 
-
-]
-               
-my_loop_function <- function(name, keyword){
-  df_keyword = keyword %>% 
-    unlist()
-  print("managed first step")
-  
-  x <- pdf_text(name) %>% 
-    as.tibble() %>% 
-    rename(content = 1)
-  x$content <- str_replace_all(x$content, c("U\\.S\\." = "US", 
-                                      "p\\.m\\." = "pm", 
-                                      "a\\.m\\." = "am", 
-                                      "d\\.c\\." = "dc"))
-  x$pagenr <- rownames(x)
-  print("managed second step")
-  y <- x %>% 
-    unnest_sentences(sentences, content)
-  print("fourth step")
-  y <- y %>% 
-    mutate(key_sentence = ifelse(str_detect(sentences, df_keyword)==T, 
-                                 sentences,
-                                 NA)) %>% 
-    filter(!is.na(key_sentence)) %>% 
-    select(pagenr, key_sentence) %>% 
-    mutate(keyword = keyword %>% 
-             as.character() %>% 
-             str_remove("c") %>% 
-             str_remove_all('"|\\(|\\)'), 
-           name = name)
-  print("fifth step")
-}
-
-library(foreach)
-
-keysentences = data.frame(pagenr = as.character(), 
-                          key_sentence = as.character(), 
-                          keywords = as.character(), 
-                          file_name = as.character())
-
-foreach(name = test$name , keyword = test$keyword) %do%
-  {
-    df_keyword = keyword %>% 
-    unlist()
-  print("managed first step")
-  
-  x <- pdf_text(name) %>% 
-    as.tibble() %>% 
-    rename(content = 1)
-  x$content <- str_replace_all(x$content, c("U\\.S\\." = "US", 
-                                            "p\\.m\\." = "pm", 
-                                            "a\\.m\\." = "am", 
-                                            "d\\.c\\." = "dc"))
-  x$content <- x$content %>% str_squish() %>% trimws()
-  x$pagenr <- rownames(x)
-  print("managed second step")
-  y <- x %>% 
-    unnest_sentences(sentences, content) %>% 
-    mutate(key_sentence = ifelse(str_detect(sentences, df_keyword)==T, 
-                                 sentences,
-                                 NA) %>% 
-             str_squish()) %>% 
-    filter(!is.na(key_sentence)) %>% 
-    select(pagenr, key_sentence) %>% 
-    mutate(keywords = paste( unlist(keyword), collapse=', '),
-           file_name = name)
-  keysentences <- bind_rows(keysentences, y)
-  print(name)
-  }
-
-missing <- anti_join(test %>% mutate(id = rownames(test)), keysentences %>% 
-                       mutate(name=file_name))
-
-foreach(name = missing$name , keyword = missing$keyword) %do%
-  {
-    df_keyword = keyword %>% 
-      unlist()
-    print("managed first step")
-    
-    x <- pdf_text(name) %>% 
-      as.tibble() %>% 
-      rename(content = 1)
-    x$content <- str_replace_all(x$content, c("U\\.S\\." = "US", 
-                                              "p\\.m\\." = "pm", 
-                                              "a\\.m\\." = "am")) 
-    x$content <- x$content %>% str_squish() %>% trimws()       
-    x$pagenr <- rownames(x)
-    print("managed second step")
-    pattern = paste(unlist(keyword), collapse='|')
-    y <- x %>% 
-      unnest_sentences(sentences, content) %>% 
-      mutate(key_sentence = ifelse(str_detect(sentences, pattern) == T, 
-                                  sentences,
-                                   NA) %>% 
-               str_squish()) %>% 
-      filter(!is.na(key_sentence)) %>% 
-      select(pagenr, key_sentence) %>% 
-      mutate(keywords = paste( unlist(keyword), collapse=', '),
-             file_name = name)
-    keysentences <- bind_rows(keysentences, y)
-    print(name)
-  }
-
-keysentences <- keysentences %>%
-  mutate(id = str_extract(file_name, "id...") %>% 
-           str_remove_all('_|id')) %>% 
-  left_join(downloadfiles) %>% 
-  select(-file_name)
-
-keysentences <- keysentences %>% 
-  mutate(key_sentence = key_sentence %>% 
-           trimws()) %>% 
-  select(-id)
-
-cols = c(1:8) %>% as.character()
-keysentences <- keysentences %>% 
-  separate(keywords, cols, sep = " ") %>% 
-  unique() %>% 
-  pivot_longer(c(3:10), names_to = "keyword") %>% 
-  select(-keyword) %>% 
-  unique() %>% 
-  filter(!is.na(value)) %>% 
-  mutate(value = value %>% 
-           str_remove_all("[,]")) %>% 
-  mutate(value = ifelse(str_detect(key_sentence, value)==T, 
-                        value, 
-                        NA)) %>% 
-  filter(!is.na(value)) %>% 
-  rename(keyword=value) %>% 
-  pivot_wider(names_from = keyword, values_from = keyword)
-
-mycols = names(keysentences %>% select(5:21))
-
-keysentences$keywords <- apply( keysentences[ , mycols ] , 1 , paste , collapse = ", " ) %>% 
-  str_remove_all(", NA|NA,|NA") %>% 
-  trimws()
-
-
-keysentences <- keysentences %>% 
-  select(-c(5:21))
-
-
-  
-keysentences <- keysentences %>% 
-  filter(!str_detect(key_sentence, "polesi|sticking to|panhandle|in the county of |milsim|being fired|was fired|getting fired|i am currently assigned|unzipped|begun|twitter handle|facebook handle|instagram handle|have investigated|as a special agent|gundersen|did knowingly, and with intent|swarmed|republican club|filed|the damage included|the capitol suffered millions|bringing andcntributing|the capitol suffered millions of dollars|fbi agent|additionally, one subject was shot and killed|violation of|§|the crowd was not lawfully authorized to enter|in light of the dangerous circumstances|entering and remaining in a restricted|engaging in physical violence in a restricted|disorderly and disruptive conducts|resisting, or impeding|civil disorder"))
-
-cleaned_riin <- read_csv('riin.csv')
-### - cleaned out manually in excel
-
-
-cleaned_riin <- cleaned_riin %>% 
-  select(-X7) %>% 
-  rename(stands_out = X2)
-
-write_csv(cleaned_riin, "cleaned_riin2.csv")
